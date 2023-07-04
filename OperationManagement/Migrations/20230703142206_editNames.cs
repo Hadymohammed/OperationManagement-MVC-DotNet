0d727b407c758329160f3a7a7706efe4975f70fe
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-07-04 05:35:19 +0300

    Fix : column names

---
+++ OperationManagement/Migrations/20230703142206_editNames.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OperationManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class editNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StartTime",
+                table: "ProductProcesses",
+                newName: "StartDate");
+
+            migrationBuilder.RenameColumn(
+                name: "EndTime",
+                table: "ProductProcesses",
+                newName: "EndDate");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Remark",
+                table: "ProductSpecifications",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Value",
+                table: "ProductMeasurements",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Unit",
+                table: "ProductMeasurements",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StartDate",
+                table: "ProductProcesses",
+                newName: "StartTime");
+
+            migrationBuilder.RenameColumn(
+                name: "EndDate",
+                table: "ProductProcesses",
+                newName: "EndTime");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Remark",
+                table: "ProductSpecifications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Value",
+                table: "ProductMeasurements",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Unit",
+                table: "ProductMeasurements",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
