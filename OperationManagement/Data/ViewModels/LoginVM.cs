076db47bb6f75bab3e50419a3b05e8974ea605d6
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-07-05 04:19:33 +0300

    Add : loginAction

---
+++ OperationManagement/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace OperationManagement.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Required,EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
