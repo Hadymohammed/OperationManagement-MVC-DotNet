09620c400d513ed055ce544e4c7e2f89bff1a18f
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-07-07 05:33:01 +0300

    refactor style

--- OperationManagement/Data/ViewModels/CreateOrderVM.cs
+++ OperationManagement/Data/ViewModels/CreateOrderVM.cs
@@ -7,7 +7,7 @@ namespace OperationManagement.Data.ViewModels
     {
         [Required]
         public Order Order { get; set; }
-        public IEnumerable<Customer>? Customers { get; set; }
+        public Customer? Customer { get; set; }
         public IEnumerable<DeliveryLocation>? DeliveryLocations { get; set; }
         public List<string>? Titles { get; set; }
         public List<IFormFile>? Attachments { get; set; }
