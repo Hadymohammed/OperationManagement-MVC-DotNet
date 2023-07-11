73df24f66614d414a2662bcfbb5fdac2e985533d
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-07-11 04:02:32 +0300

    add : enterpriseId

--- OperationManagement/Data/ViewModels/CreateOrderVM.cs
+++ OperationManagement/Data/ViewModels/CreateOrderVM.cs
@@ -7,6 +7,8 @@ namespace OperationManagement.Data.ViewModels
     {
         [Required]
         public Order Order { get; set; }
+        [Required]
+        public int EnterpriseId { get; set; }
         public Customer? Customer { get; set; }
         public IEnumerable<DeliveryLocation>? DeliveryLocations { get; set; }
         public List<string>? Titles { get; set; }
