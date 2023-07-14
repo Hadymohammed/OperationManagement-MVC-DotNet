ad18f8c8190fb741cf4b1bebdb9aacaaa73625c8
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-07-15 02:52:57 +0300

    fix : index action to get only user's enterprise categories

--- OperationManagement/Controllers/ProcessCategoriesController.cs
+++ OperationManagement/Controllers/ProcessCategoriesController.cs
@@ -33,7 +33,10 @@ namespace OperationManagement.Controllers
         // GET: ProcessCategories
         public async Task<IActionResult> Index()
         {
-            return View(await _categoryService.GetAllAsync());
+            var categories = await _categoryService.GetAllAsync();
+            var user = await _userManager.GetUserAsync(User);
+
+            return View(categories.Where(c=>c.EnterpriseId==(int)user.EnterpriseId));
         }
 
         // GET: ProcessCategories/Details/5
