a14c7156671be88d66b791a83abb0b8f6205eae8
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-07-14 03:56:15 +0300

    remove role input from login

--- OperationManagement/Controllers/AccountController.cs
+++ OperationManagement/Controllers/AccountController.cs
@@ -41,7 +41,7 @@ namespace OperationManagement.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = await _userManager.FindByEmailAsync(vm.Email);
+                var user = await _userManager.FindByEmailAsync(vm.Email);   
                 if (user == null)
                 {
                     ModelState.AddModelError("Email", "This email not registered on our system.");
@@ -56,7 +56,7 @@ namespace OperationManagement.Controllers
                 var result = await _signInManager.PasswordSignInAsync(user, vm.Password, false, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
-                    if (vm.Role == UserRoles.User)
+                    if (await _userManager.IsInRoleAsync(user, UserRoles.User))
                     {
                         return RedirectToAction("Index", "Customers");
                     }
